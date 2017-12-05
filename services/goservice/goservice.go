64eafc86804d08b37dbd8c57b7b13730d3b42dfb
Thanasis Tryfonos <thanasistryfonos@gmail.com>
2017-12-05 14:46:52 +0200

    created a simple go rest service

---
+++ services/goservice/goservice.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/product", getHelloWorld).Methods("GET")
+	r.HandleFunc("/product", postHelloWorld).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8010", r))
+}
+
+func getHelloWorld(res http.ResponseWriter, req *http.Request) {
+	res.Write([]byte("[GET]: Hello world from Go!"))
+}
+
+func postHelloWorld(res http.ResponseWriter, req *http.Request) {
+	res.Write([]byte("[POST]: Hello world from Go!"))
+}
