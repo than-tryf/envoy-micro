4aaaaef472c68f151486d57c2b46263f36a4921c
Thanasis Tryfonos <thanasistryfonos@gmail.com>
2017-12-05 16:39:54 +0200

    started jservice project

---
+++ services/jservice/src/main/java/eu/thanasistryfonos/jservice/JserviceApplication.java
@@ -0,0 +1,12 @@
+package eu.thanasistryfonos.jservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JserviceApplication.class, args);
+	}
+}
