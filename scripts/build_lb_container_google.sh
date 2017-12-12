318deaa660fddc1d0293f68b92eefaf1db05f10e
Thanasis Tryfonos <thanasistryfonos@gmail.com>
2017-12-12 16:42:29 +0200

    testing monitoring agent (envoy)

--- scripts/build_lb_container_google.sh
+++ scripts/build_lb_container_google.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-docker build -t loadbalancer-google . -f Dockerfile-loadbalancer-google
-docker run -d -p 8000:80 -p 9001:9001 --network=envoymesh --net-alias=load-balancer-google --name loadbalancer-google loadbalancer-google 
+#docker build -t loadbalancer-google . -f Dockerfile-loadbalancer-google
+docker build -t loadbalancer-google . -f Dockerfile-loadbalancer-google-monit
+docker run -d -p 8000:80 -p 9001:9001 --network=envoymesh --net-alias=load-balancer-google --name loadbalancer-google loadbalancer-google
---
+++ scripts/tests/start_monservices.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# Start Java
+nohup java -jar /home/envoymonagent.jar 2000 1>/dev/null 2>/dev/null &
+/usr/local/bin/envoy -c /etc/envoy.json
