60a0c2c8c6939a4a75e64ffe73688b1a34df5117
Thanasis Tryfonos <thanasistryfonos@gmail.com>
2017-12-15 12:38:18 +0200

    restructured the folders (scripts) folder

--- build-servicemesh-test.sh
+++ build-servicemesh-test.sh
@@ -13,7 +13,7 @@ cd ..
 # Building
 
 docker run -d --network=envoymesh --net-alias=my-rabbit --hostname my-rabbit --name some-rabbit -p 8080:15672 rabbitmq:3-management
-
+# docker run -d -it --dns localhost -e CONSUL_HTTP_ADDR=localhost:8500 -e CONSUL_RPC_ADDR=localhost:8400 --hostname my-rabbit --name some-rabbit -p 8080:15672 rabbitmq:3-management
 
 #The Google load balancer
 cp envoy-config/load-balancer-google.json dockerfiles/load-balancer-google.json
